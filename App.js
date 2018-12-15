7598fd66a09652fc7afb583ef358d33008202ccd
Andy Poon <anpoon430@gmail.com>
2018-12-15 02:28:08 -0500

    support for hooks and added record button

--- App.js
+++ App.js
@@ -1,40 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Fragment} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { Button } from 'react-native-elements';
 import { MapView} from 'expo';
 
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    position: 'absolute',
+    left: 0, right: 0, bottom: '5%'
   },
+  recordBtn:{
+    borderColor: 'white',
+    borderWidth:5,
+    backgroundColor:'red',
+    width:70,
+    height:70
+  }
 });
 
 
 export default function App(){
 
-
+  const handleRecord = (evt) => {
+    console.log(evt);
+  }
   return (
-    <MapView
-    style={{ flex: 1 }}
-    provider="google"
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-    followsUserLocation
-    showsMyLocationButton
-    showsUserLocation
-  />
-    // <View style = {styles.container}>
-    //   <Text>
-    //     Some stuff
-    //   </Text>
-    // </View>
+    <Fragment>
+      <MapView
+      style={{ flex: 1 }}
+      provider="google"
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      followsUserLocation
+      showsMyLocationButton
+      showsUserLocation
+    />
+      <View
+        style = {styles.container}
+        pointerEvents='box-none'
+        >
+        <Button
+          buttonStyle= {styles.recordBtn}
+          onPress={handleRecord}
+          raised
+          borderRadius={500}
+          />
+      </View>
+    </Fragment>
   )
 }
 
